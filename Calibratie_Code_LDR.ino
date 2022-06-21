61b00981a450e109c1e3548b1038d3642c77c051
Rick1Perquin <92263896+Rick1Perquin@users.noreply.github.com>
2022-06-21 18:27:41 +0200

    nieuwe pins

--- Calibratie_Code_LDR.ino
+++ Calibratie_Code_LDR.ino
@@ -1,8 +1,8 @@
 
-const int IRPinBand = 43; //de echopin van de ultrasoon voor de bakjes orientatie
-const int bandMotorPin = 31; //de pin die aangesloten staat op de MOSFET van de bandmotor
-const int LDRPin = A15; //de pin die is aangesloten op de LDR
-const int comPinIn = 22; //de pin die wordt gebruikt om te communiceren naar de andere arduino (in)
+const int IRPinBand = 53; //de echopin van de ultrasoon voor de bakjes orientatie
+const int bandMotorPin = 36; //de pin die aangesloten staat op de MOSFET van de bandmotor
+const int LDRPin = A7; //de pin die is aangesloten op de LDR
+const int comPinIn = 2; //de pin die wordt gebruikt om te communiceren naar de andere arduino (in)
 
 // Alle globale variabele worden hier gedefineerd:
 int LDRWaarde;
@@ -35,7 +35,7 @@ void loop() {
   
   while(IRBandStatus == 0){
   Serial.println("Er staat een bakje:");
-  delay(100);
+  delay(80);
   digitalWrite(bandMotorPin, LOW);
   delay(1000);
   LDRWaarde = analogRead(LDRPin); //LDR wordt uitgelezen
